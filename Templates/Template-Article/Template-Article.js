e2f176805cf88a0ec5f775fa2afae3076e2d185c
GarfieldGod <1351763110@qq.com>
2023-06-05 21:26:41 +0800

    2.0.4
    Signed-off-by: GarfieldGod <1351763110@qq.com>

---
+++ Templates/Template-Article/Template-Article.js
@@ -0,0 +1,6 @@
+function viewavatar(){
+    document.getElementById("avatarviewer").style.display = 'block';
+}
+function ESCviewavatar(){
+    document.getElementById("avatarviewer").style.display = 'none';
+}
