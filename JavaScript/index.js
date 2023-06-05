afbe9ebeef3983b753a8f4221a0303a55deb7de5
GarfieldGod <1351763110@qq.com>
2023-06-05 20:20:17 +0800

    2.0.3
    Signed-off-by: GarfieldGod <1351763110@qq.com>

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,10 +5,22 @@ function ESCviewavatar(){
     document.getElementById("avatarviewer").style.display = 'none';
 }
 function CoverBe(){
-    /* document.getElementById("cover").style.height = 0 + 'px'; */
-    document.getElementById("cover").className = "cover";
-    /*document.getElementById("cover").style.height = 430 + "px";*/
-    document.getElementById("contentbackground").style.display = 'block';
-    document.getElementById("thefooter").style.display = 'block';
-    document.body.scrollTop=document.documentElement.scrollTop=430;
+    if(document.getElementById("cover").className == "entercover")
+    {
+        document.getElementById("cover").className = "cover";
+        document.getElementById("contentbackground").style.display = 'block';
+        document.getElementById("thefooter").style.display = 'block';
+        document.getElementById("navigationBlockB").style.marginLeft = 0 + '%';
+        document.getElementById("TheNavigation").style.marginRight = 0 + 'px';
+        document.body.scrollTop=document.documentElement.scrollTop=430; 
+    }
+    else
+    {
+        document.getElementById("cover").className = "entercover";
+        document.getElementById("contentbackground").style.display = 'none';
+        document.getElementById("thefooter").style.display = 'none';
+        document.getElementById("navigationBlockB").style.marginLeft = 20 + '%';
+        document.getElementById("TheNavigation").style.marginRight = -300 + '%';
+        document.body.scrollTop=document.documentElement.scrollTop=0; 
+    }
 }
